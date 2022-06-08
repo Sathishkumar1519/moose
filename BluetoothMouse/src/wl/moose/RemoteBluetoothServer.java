a71a004227511590c4acf4c76d76e9ebe1c4a9ad
Sathishkumar1519 <106546592+Sathishkumar1519@users.noreply.github.com>
2022-06-08 13:18:04 +0530

    Add files via upload

---
+++ BluetoothMouse/src/wl/moose/RemoteBluetoothServer.java
@@ -0,0 +1,9 @@
+package wl.moose;
+
+public class RemoteBluetoothServer{
+
+    public static void main(String[] args) {
+        Thread waitThread = new Thread(new WaitThread());
+        waitThread.start();
+    }
+}
